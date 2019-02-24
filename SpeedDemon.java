1054193a2a9e7a618c6c35930528916472894064
Luke Jones <46899472+Jonesl13@users.noreply.github.com>
2019-02-24 19:16:08 +0000

    Update SpeedDemon.java

--- SpeedDemon.java
+++ SpeedDemon.java
@@ -3,7 +3,7 @@ public class SpeedDemon extends NPC
 {
 	public SpeedDemon(float xPixelsPerSecond, float yPixelsPerSecond,int r,String diffculty, Background background)
 	{
-		super(xPixelsPerSecond, yPixelsPerSecond, r, "Enemies/enemy2.png",background);
+		super(xPixelsPerSecond, yPixelsPerSecond, r, "Enemies/Death/enemy2.png",background);
 		ID = "SpeedDemon";
 		type = new String[]{"warp"};
 		defaultType = new String[]{"warp"};
