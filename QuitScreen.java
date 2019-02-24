684d7000d0d4cb31eee21e068c5b0780a2cbeb1d
jordanyoung630 <37487946+jordanyoung630@users.noreply.github.com>
2019-02-24 15:45:34 +0000

    Update QuitScreen.java
    Added method for yes/no button functionality.

--- QuitScreen.java
+++ QuitScreen.java
@@ -3,15 +3,47 @@
 public class QuitScreen{
 	private static String ImageFile = "quitScreen.png";
 	private static ImageAct quitScreenWindow;
-	private static GenButton rects[];
+	private static GenButton buttons[];
 	
 	public QuitScreen() {
-		buttonBox = new GenButton[2];
+		quitScreenWindow = new ImageAct(ImageFile);
+		buttons = new GenButton[2];
 		
-		for (int i=0; i<2; i++)
-			buttonBox[i] = new GenButton(90, 30, Color.RED, 500);
+		for (GenButton button: buttons)
+			buttons[i] = new GenButton(90, 30, Color.RED, 500);
 
-		buttonBox[0].setLocation((float) 140, (float) 140);
-		buttonBox[1].setLocation((float) 288, (float) 140);
+		quitScreenWindow.setLocation((float) 256, (float) 288);
+		buttons[0].setLocation((float) 140, (float) 140);
+		buttons[1].setLocation((float) 288, (float) 140);
+	}
+	
+	public void display(RenderWindow window) {
+		quitScreenWindow.draw(window);
+		for (GenButton button: buttons)
+			button.draw(window);
+	}
+	
+	public boolean yesEvent(Vector2i mouseLoc) {
+		boolean detected = false;
+		
+		if (buttons[0].detectPos(buttons[0].getRectPosition(), buttons[0].getRectDimensions(), mouseLoc)) {
+			detected = true;
+			buttons[0].setRectColor(Color.TRANSPARENT, 0);
+		}
+		else buttons[0].setRectColor(Color.GREEN, 500);
+
+		return detected;
+	}
+	
+	public boolean noEvent(Vector2i mouseLoc) {
+		boolean detected = false;
+		
+		if (buttons[1].detectPos(buttons[1].getRectPosition(), buttons[1].getRectDimensions(), mouseLoc)) {
+			detected = true;
+			buttons[1].setRectColor(Color.TRANSPARENT, 0);
+		}
+		else buttons[1].setRectColor(Color.RED, 500);
+		
+		return detected;
 	}
 }
