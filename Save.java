3239d1c3d992abfcbe355045beb3401f69cbc8d8
Luke Jones <46899472+Jonesl13@users.noreply.github.com>
2019-03-01 14:41:22 +0000

    Commenting

--- Save.java
+++ Save.java
@@ -8,10 +8,13 @@ import org.jsfml.graphics.*;
 public class Save {
 	
 	private String diff;
-	private String saveFile = "save.txt";
+	private String saveFile = "save.txt"; //Directory of the save file to read
 	private int currLvl, currRound;
 	private RenderWindow r;
 	
+	/**
+	* Constructor that reads the save.txt file when initialised
+	*/
 	public Save(){
 			readFile();
 		
@@ -22,6 +25,10 @@ public class Save {
 		currLvl++;
 	}
 	
+	/**
+	* Set the current level to the given int newLevel
+	* @param newLevel The new int value of the current level
+	*/
 	public void setLevel(int newLevel)
 	{
 		currLvl = newLevel;
@@ -42,12 +49,20 @@ public class Save {
 		return diff;
 	}
 	
+	/**
+	* Changes the current difficulty to the given String diff
+	* @param diff The new difficulty to set in String
+	*/
 	public void setDiff(String diff){
 	
 		this.diff = diff;
 		saveFile(currLvl, currRound, diff);
 	}
 	
+	/**
+	* Called when the user wishes to create a new save file to the given difficulty
+	* @param diff The difficulty in String that the user wishes to save at 
+	*/
 	public void newGame(String diff){
 		
 		currLvl = 1;
@@ -56,8 +71,9 @@ public class Save {
 		setDiff(diff);
 				
 	}
-	
-	//Load the save and continue the game from the previous Level and round
+	/**
+	* Load the save and continue the game from the previous Level and round
+	*/
 	public void contGame(){
 		
 		if ((currLvl < 1) || (currRound < 1) || (diff.isEmpty())){
@@ -67,8 +83,12 @@ public class Save {
 			diff.equals("easy");
 		}			
 	}
-	
-	//Saves the current values to the .txt file
+	/**
+	* Saves the current values to the .txt file
+	* @param currLvl The current level to save 
+	* @param currRound The current round to save
+	* @param diff The difficulty in String to save
+	*/
 	public void saveFile(int currLvl, int currRound, String diff){
 	
 		try{
@@ -87,7 +107,9 @@ public class Save {
 	
 	}
 	
-	//Reads the values from the save.txt file and updates the local variables
+	/**
+	* Reads the values from the save.txt file and updates the local variables
+	*/
 	private void readFile(){
 	
 		try{
